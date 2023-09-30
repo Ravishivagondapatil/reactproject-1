b02feb98b8371874daa053f14b314782cf851354
Ravi Patil <111782129+Ravishivagondapatil@users.noreply.github.com>
2023-09-30 01:51:15 -0700

    Add files via upload

---
+++ project-1/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import HeroSection from "./components/Hero.jsx";
+import Navigation from "./Components/Navigation";
+const App=()=>{
+    return(
+        <div>
+   <Navigation/>
+   <HeroSection/>
+    </div>
+    );
+};
+export default App;
